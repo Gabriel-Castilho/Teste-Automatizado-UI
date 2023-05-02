996cabf08055db0d2e5dbc7752a600c34db94a36
Gabriel Castilho <gabriel.castilho@outlook.com.br>
2023-05-02 17:33:46 -0300

    removendo comentários desnecessários

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -24,29 +24,5 @@ describe('Funcionalidade Login', () => {
         cy.login('useruseruserinvalid@ebac.co', 'asdadsdasadsas');
         cy.get('.woocommerce-error').should('contain','Endereço de e-mail desconhecido');
     })
-
-
-    /* Adicionar produto
-    cy.adicionarProduto('Sauce Labs Bike Light');
-    cy.adicionarProduto('Sauce Labs Fleece Jacket');
-    cy.adicionarProduto('Sauce Labs Backpack');
-    
-    //Ir para o carrinho
-    cy.get('.shopping_cart_link').click();
-    cy.get('[data-test="checkout"]').click();
-
-    //Efetuar pagamento
-    cy.cadastro('Gabriel','Castilho','12092856');
-
-    //Concluir Pagamento
-    cy.get('[data-test="finish"]').click();
-    cy.get('.complete-header').should('contain', 'Thank you for your order')
-    //Voltar a home
-   //cy.get('[data-test="back-to-products"]').click();
-
-
-    */
-
-
 })
 
